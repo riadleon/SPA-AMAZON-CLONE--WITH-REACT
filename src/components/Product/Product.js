e4311af8d6ec9542241e82aa253d3eb6a9e9971c
riadshalahinleon <riadshalahinleon@gmail.com>
2022-09-25 18:54:54 +0600

    consoling onclick product item

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,22 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const {name , img, seller, price, ratings} = props.product;
+    const { name, img, seller, price, ratings } = props.product;
+    const {handleAddToCart} = props
+
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className="product-info">
+                <p className='product-name'>{name}</p>
+                <p>Price : ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Rating: {ratings} Stars</small></p>
+            </div>
+            <button onClick={ () => props.handleAddToCart(props.product)} className='btn-cart'>
+                <p>Add to cart</p>
+            </button>
+
         </div>
     );
 };
