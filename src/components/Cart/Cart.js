41a6641fcae5c81df11e9f5fda284b1b4c2f114b
riadshalahinleon <riadshalahinleon@gmail.com>
2022-09-26 13:43:24 +0600

    Calculated shopping cart price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './Cart.css';
 
-const Cart = (props) => {
+const Cart = ({ cart }) => {
+    // const 
+    let total = 0;
+    let shipping = 0;
+
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+
+    }
+    const tax = (total * 0.1).toFixed(2);
     return (
-        <div>
+        <div className='cart'>
             <h4>Order summary</h4>
-            <p>Selected item: {props.cart.length}</p>
+            <p>Selected item: {cart.length}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total: </h5>
         </div>
     );
 };
