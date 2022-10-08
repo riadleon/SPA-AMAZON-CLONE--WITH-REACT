ceed6fd97228c7f3d7eddb31f1c66e1aa5339f4d
riadshalahinleon <riadshalahinleon@gmail.com>
2022-10-08 10:34:28 +0600

    React Router setup and intro to order review page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../images/Logo.svg'
 import './Header.css'
 
@@ -7,10 +8,10 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
-                <a href="/Shop">Shop</a>
-                <a href="/Orders">Orders</a>
-                <a href="/Inventory">Inventory</a>
-                <a href="/About">About</a>
+                <Link to="/">Shop</Link>
+                <Link to="/Orders">Orders</Link>
+                <Link to="/Inventory">Inventory</Link>
+                <Link to="/About">About</Link>
             </div>
         </nav>
     );
