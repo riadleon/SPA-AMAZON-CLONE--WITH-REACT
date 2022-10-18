c940f4c19b547dc0aa89fe64483847beb4028d97
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-10-19 03:23:05 +0600

    Login Components

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is Login</h2>
+        <div className='form-container'>
+            <h2 className='form-tittle'>Login</h2>
+            <form >
+                <div className="form-control">
+                    <label htmlFor='email'>Email</label>
+                    <input type="email" name="email" id="" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor='password'>Password</label>
+                    <input type="password" name="password" id="" required />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <p>New here? No problem <Link to='/signup'> <small>Create a new Account</small> </Link> </p>
         </div>
     );
 };
