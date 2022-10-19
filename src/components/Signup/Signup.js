3c559c6f0a3fc625155a17f269421ad204f8d935
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-10-19 11:56:51 +0600

    Get Form Data and display form error and created user state in context

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/UserContext';
 import './Signup.css'
 
 const Signup = () => {
+    const [error, setError] = useState(null)
+    const { createUser, signInWithGoogle } = useContext(AuthContext)
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const comfirm = form.confirmpass.value;
+        console.log(email, password, comfirm);
+
+        if (password.length < 6) {
+            setError('Password should be 6 character or more')
+            return;
+        }
+
+        if (password !== comfirm) {
+            setError('Your Password is incorrect');
+            return;
+        }
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log('Registered user:', user);
+            })
+            .catch(error => {
+                console.error('error:', error);
+            })
+
+    }
+
+
     return (
         <div>
             <div className='form-container'>
                 <h2 className='form-tittle'>Sign up</h2>
-                <form >
+                <form onSubmit={handleSubmit}>
                     <div className="form-control">
                         <label htmlFor='email'>Email</label>
                         <input type="email" name="email" id="" required />
@@ -18,11 +51,12 @@ const Signup = () => {
                     </div>
                     <div className="form-control">
                         <label htmlFor='confirmpass'>Confirm Password</label>
-                        <input type="confirmpass" name="confirmpass" id="" required />
+                        <input type="password" name="confirmpass" id="" required />
                     </div>
                     <input className='btn-submit' type="submit" value="Sign up" />
                 </form>
                 <p>Already Have an Account?<Link to='/login'> <small>Login</small> </Link> </p>
+                <p className='text-error'> {error} </p>
             </div>
         </div>
     );
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,17 +1,54 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
+import app from '../firebase/firebase.init'
 
+const auth = getAuth(app);
 
 export const AuthContext = createContext();
 
-const UserContext = ({children}) => {
+const UserContext = ({ children }) => {
+
+    const [user, setUser] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    const googleProvider = new GoogleAuthProvider();
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
 
 
-const authInfo = {}
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
 
+    //
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false)
+            console.log('auth state changed', currentUser);
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+    const signInWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
+
+
+    const authInfo = { user, createUser, signIn, logOut, signInWithGoogle, loading };
     return (
-       <AuthContext.Provider value={authInfo}>
+        <AuthContext.Provider value={authInfo}>
             {children}
-       </AuthContext.Provider>
+        </AuthContext.Provider>
     );
 };
 
